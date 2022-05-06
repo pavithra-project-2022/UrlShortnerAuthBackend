343807c72b145cb14a942f44f7624e753f2d0a3e
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-05-06 23:59:47 +0530

    my first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path')
 const app = express();
 const cors = require("cors");
 const connection = require('./db')
